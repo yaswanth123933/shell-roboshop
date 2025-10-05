118d7914c0429d55874868ffab7db27bbcdf132c
yaswanth <yaswanthch764@gmail.com>
2025-10-05 09:06:40 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-04ef25cbf88a4d0ab"
+ZONE_ID="Z00544453B01ZBG0FCY74"
+DOMAIN_NAME="daws85s.store"
 
 for instance in $@
 do
@@ -11,16 +13,38 @@ do
     if [ $instance != "frontend" ]; then
         Ip=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0]. 
         PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" # it becomes mongodb.daws85s.store
         
     else
         
         Ip=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].
         PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME" #daws85s.store
         
     fi
 
     echo "$instance: $IP"
+
+    aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+  {
+    "Comment": "Updating record set"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$RECORD_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP'"
+        }]
+      }
+    }]
+  }
+  '
 done
     
+
 
 
