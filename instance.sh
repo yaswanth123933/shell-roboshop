eb537b7237fe7107f0621f1eda9474cf60c3c1c5
yaswanth <yaswanthch764@gmail.com>
2025-10-06 07:41:12 +0530

    instance

--- instance.sh
+++ instance.sh
@@ -4,19 +4,30 @@
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-04ef25cbf88a4d0ab"
 
-for instance in $@
-do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+for instance in $@; do
+  INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type t3.micro \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text)
 
-    #get private ip
-    if [ "$instance" = "frontend" ]; then
-        Ip=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-        
-    else
-        
-        Ip=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-        
-    fi
+  #  Wait until instance is running
+  aws ec2 wait instance-running --instance-ids $INSTANCE_ID
 
-    echo "$instance: $IP"
+  #  Get IP based on instance name
+  if [ "$instance" = "frontend" ]; then
+    IP=$(aws ec2 describe-instances \
+      --instance-ids $INSTANCE_ID \
+      --query 'Reservations[0].Instances[0].PublicIpAddress' \
+      --output text)
+  else
+    IP=$(aws ec2 describe-instances \
+      --instance-ids $INSTANCE_ID \
+      --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+      --output text)
+  fi
+
+  echo "$instance: $IP"
 done
