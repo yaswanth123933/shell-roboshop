de925ea65e668fbf636db9b70afdd4d5f3c474b2
yaswanth <yaswanthch764@gmail.com>
2025-10-06 07:48:09 +0530

    instance

--- instance.sh
+++ instance.sh
@@ -3,6 +3,8 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-04ef25cbf88a4d0ab"
+ZONE_ID="Z00544453B01ZBG0FCY74"
+DOMAIN_NAME="daws85s.store"
 
 for instance in $@; do
   INSTANCE_ID=$(aws ec2 run-instances \
@@ -22,12 +24,37 @@ for instance in $@; do
       --instance-ids $INSTANCE_ID \
       --query 'Reservations[0].Instances[0].PublicIpAddress' \
       --output text)
+      RECORD_NAME="$instance.$DOMAIN_NAME"
   else
     IP=$(aws ec2 describe-instances \
       --instance-ids $INSTANCE_ID \
       --query 'Reservations[0].Instances[0].PrivateIpAddress' \
       --output text)
+      RECORD_NAME="$DOMAIN_NAME"
   fi
 
   echo "$instance: $IP"
+  aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+  {
+    "Comment": "Updating record set"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$RECORD_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP'"
+        }]
+      }
+    }]
+  }
+  '
 done
+    
+
+
+
+
