fff492c62dc80897617872e82520fcdf8aa50672
yaswanth <yaswanthch764@gmail.com>
2025-10-06 06:37:13 +0530

    instance

--- instance.sh
+++ instance.sh
@@ -6,10 +6,10 @@ SG_ID="sg-04ef25cbf88a4d0ab"
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value='$instance'}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0] .InstanceId' --output text)
 
     #get private ip
-    if [ $instance != "frontend" ]; then
+    if [ "$instance"!= "frontend" ]; then
         Ip=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
         
     else
