765204b4509b451d05856fa87992631423ffc853
yaswanth <yaswanthch764@gmail.com>
2025-10-07 08:11:08 +0530

    mongodb

--- catalogue.sh
+++ catalogue.sh
@@ -41,7 +41,7 @@ dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Installing NodeJS"
 
 id roboshop &>>$LOG_FILE
-if [ $?-ne 0 ]; then
+if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
     VALIDATE $? "Creating system user"
 else
