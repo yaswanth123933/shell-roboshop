ded0498c876610ff2d63c32b30aaaca5076bdaf4
yaswanth <yaswanthch764@gmail.com>
2025-10-10 09:31:45 +0530

    instance

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -43,5 +43,5 @@ rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
 VALIDATE $? "Setting up permissions"
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $start_TIME))
+TOTAL_TIME=$(( $END_TIME - $START_TIME))
 echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
---
+++ user.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+USERID=$(id -u)
+ R="\e[31m"
+ G="\e[32m"
+ Y="\e[33m"
+ N="\e[0m"
+ LOGS_FOLDER="/var/log/shell-roboshop"
+ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+ SCRIPT_DIR=$PWD
+ MONGODB_HOST=mongodb.daws85s.store
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 #failure is other than zero
+fi
+
+VALIDATE(){ #functions receive inputs through argus just like script argus
+   if [ $1 -ne 0 ];then
+      echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+
+      exit 1
+   else
+      echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+
+
+   fi
+}
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling NodeJS"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enbling NodeJS 20"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing NodeJS"
+
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating system user"
+else
+    echo -e "User already exist ... $Y SKIPPING $N"
+fi
+
+mkdir -p /app
+VALIDATE $? "Creating app directory" 
+
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading user application"
+
+cd /app
+VALIDATE $? "Changing to app directory"
+
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
+
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "Unzip user"
+
+npm install &>>$LOG_FILE
+VALIDATE $? "Install dependencies"
+
+cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service
+VALIDATE $? "Copy systemctl service"
+
+systemctl daemon-reload
+systemctl enable user &>>$LOG_FILE
+VALIDATE $? "Enable user" 
+
+systemctl start user
+VALIDATE $? "Restarted user"
+
+
+
