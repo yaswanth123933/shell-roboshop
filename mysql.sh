ad54ade7dc6fb86347d789fc351cb6c76ae780d1
yaswanth <yaswanthch764@gmail.com>
2025-10-10 08:48:27 +0530

    instance

--- mysql.sh
+++ mysql.sh
@@ -8,7 +8,7 @@ USERID=$(id -u)
  LOGS_FOLDER="/var/log/shell-roboshop"
  SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-START_TIME=$(date +%S)
+
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
@@ -29,21 +29,11 @@ VALIDATE(){ #functions receive inputs through argus just like script argus
    fi
 }
 
-dnf module disable redis -y  &>>$LOG_FILE
-VALIDATE $? "Disabling Default Redis"
-dnf module enable redis:7 -y  &>>$LOG_FILE
-VALIDATE $? "Enabling Redis 7"
-dnf install redis -y   &>>$LOG_FILE
-VALIDATE $? "Installing Redis"
+dnf install mysql-server -y
+systemctl enable mysqld
+systemctl start mysqld  
+mysql_secure_installation --set-root-pass RoboShop@1
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-VALIDATE $? "Allowing Remote connections to Redis"
-
-systemctl enable redis  &>>$LOG_FILE
-VALIDATE $? "Enabling Redis"
-systemctl start redis  &>>$LOG_FILE
-VALIDATE $? "Starting Redis"
- 
 END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $start_TIME ))
+TOTAL_TIME=$(( $END_TIME - $start_TIME))
 echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
