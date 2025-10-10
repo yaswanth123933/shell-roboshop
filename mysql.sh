edc6125b8017fc98bdba59f8fdfcef2444bc4957
yaswanth <yaswanthch764@gmail.com>
2025-10-10 08:59:02 +0530

    instance

--- mysql.sh
+++ mysql.sh
@@ -8,7 +8,7 @@ USERID=$(id -u)
  LOGS_FOLDER="/var/log/shell-roboshop"
  SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-
+START_TIME=$(DAte +%s)
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
@@ -30,9 +30,13 @@ VALIDATE(){ #functions receive inputs through argus just like script argus
 }
 
 dnf install mysql-server -y
+VALIDATE $? "Installing MySQL Server"
 systemctl enable mysqld
+VALIDATE $? "Enabling MySQL Server"
 systemctl start mysqld  
+VALIDATE $? "Starting MySQL Server"
 mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "Setting up Root password"
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $start_TIME))
