db8b563765413d838b559248400c01db9ab51b45
yaswanth <yaswanthch764@gmail.com>
2025-10-10 08:53:12 +0530

    instance

--- redis.sh
+++ redis.sh
@@ -8,7 +8,7 @@ USERID=$(id -u)
  LOGS_FOLDER="/var/log/shell-roboshop"
  SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-START_TIME=$(date +%S)
+START_TIME=$(date +%s)
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
